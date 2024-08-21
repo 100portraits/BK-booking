eb752ff737475fe1d146715eb3bcad1ffdb01371
sahir <sahirde@gmail.com>
2024-08-21 13:40:55 +0200

    experimental long polling pt3

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // src/firebase.js
 
 import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
 import { getFirestore, initializeFirestore } from "firebase/firestore";
 
 // Your environment variables from VSCODE
