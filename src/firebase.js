d18833f4ec53f9a798e33e2c417765e932d8d348
sahir <sahirde@gmail.com>
2024-08-21 11:44:41 +0200

    env variables firebase

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// src/firebase.js
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your environment variables from Cloudflare Pages
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
